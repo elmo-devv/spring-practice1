f96530c336acc0fe38904e772f84ec83510003fa
elmo-devv <ismyelmo@gmail.com>
2022-10-04 00:21:26 +0900

    feat: DI원칙을 지키며 코딩

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,23 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 할인 정책을 변경
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 구체화에 의존, DIP 위반!
+    private final DiscountPolicy discountPolicy;  //DIP 를 지키니 구체 클래스가 없어서 돌아가지 않는다.
+
+    // interface 에만 의존한다.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
