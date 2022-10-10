255a4f4349bcdd826f3197abee8683ce4305a032
elmo-devv <ismyelmo@gmail.com>
2022-10-11 00:29:11 +0900

    feat: bean test code

---
+++ src/test/java/hello/core/binfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,39 @@
+package hello.core.binfind;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    void 빈_이름으로_조회() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    void 빈타입으로조회() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    void 구채_타입으로_조회() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    void 빈이름으로조회안됨() {
+        assertThatThrownBy(() -> {
+            ac.getBean("xxxx", MemberService.class);
+        }).isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+
+}
---
+++ src/test/java/hello/core/binfind/ApplicationContextInfoTest.java
@@ -0,0 +1,40 @@
+package hello.core.binfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    void 모든빈출력하기() throws Exception {
+        //given
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //when
+        for(String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+        }
+        //then
+    }
+
+    @Test
+    void 만든빈출력하기() throws Exception {
+        //given
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //when
+        for(String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            //Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+            }
+        }
+        //then
+    }
+}
