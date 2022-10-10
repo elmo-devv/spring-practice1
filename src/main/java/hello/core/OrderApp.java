ff8089599391ba8cb5396465bd42a602d948f5c6
elmo-devv <ismyelmo@gmail.com>
2022-10-10 23:41:36 +0900

    feat: spring 적용

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import org.springframework.context.ApplicationContext;
@@ -11,12 +12,17 @@ public class OrderApp {
 
     public static void main(String[] args) {
 
+        //spring 적용, 컨테이너에서 bean 설정 되어 있는 것을 꺼내온다.
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        System.out.println("order = " + order);
     }
 }
